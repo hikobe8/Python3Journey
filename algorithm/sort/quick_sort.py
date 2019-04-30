0382802376ad31172384de23556d2ec683581a18
hikobe8 <hikobe8@163.com>
2019-04-30 17:00:25 +0800

    quick sort

--- algorithm/sort/quick_sort.py
+++ algorithm/sort/quick_sort.py
@@ -1,36 +1,80 @@
 # 快速排序 Python实现
+import random as rd
 
 
 # 递归实现快速排序
 def quick_sort(un_sorted_arr, left, right):
-    o_l = left
-    o_r = right
     if left >= right or left < 0:
         return
-    sIndex = right
-    standard = un_sorted_arr[sIndex]
-    right = o_r - 1
+    standard = un_sorted_arr[right]
+    i = left
+    j = right - 1
     while True:
 
-        while un_sorted_arr[right] > standard:
-            right -= 1
-        while un_sorted_arr[left] < standard:
-            left += 1
+        while un_sorted_arr[j] > standard:
+            j -= 1
+        while un_sorted_arr[i] < standard:
+            i += 1
 
-        if left < right:
-            un_sorted_arr[left] = un_sorted_arr[left] ^ un_sorted_arr[right]
-            un_sorted_arr[right] = un_sorted_arr[left] ^ un_sorted_arr[right]
-            un_sorted_arr[left] = un_sorted_arr[left] ^ un_sorted_arr[right]
+        if i < j:
+            un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[j]
+            un_sorted_arr[j] = un_sorted_arr[i] ^ un_sorted_arr[j]
+            un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[j]
         else:
             break
-    if left < sIndex:
-        un_sorted_arr[left] = un_sorted_arr[left] ^ un_sorted_arr[sIndex]
-        un_sorted_arr[sIndex] = un_sorted_arr[left] ^ un_sorted_arr[sIndex]
-        un_sorted_arr[left] = un_sorted_arr[left] ^ un_sorted_arr[sIndex]
-    quick_sort(un_sorted_arr, o_l, left - 1)
-    quick_sort(un_sorted_arr, left + 1, o_r)
+    if i < right:
+        un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[right]
+        un_sorted_arr[right] = un_sorted_arr[i] ^ un_sorted_arr[right]
+        un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[right]
+    quick_sort(un_sorted_arr, left, i - 1)
+    quick_sort(un_sorted_arr, i + 1, right)
 
 
-arr = [-100, 200, -5, 4000, 100, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10000, -1232131, 2343, 0, 1, 8]
-quick_sort(arr, 0, len(arr) - 1)
-print(arr)
+# 循环实现快速排序
+def quick_sort_iteration(un_sorted_arr):
+    left = 0
+    right = len(un_sorted_arr) - 1
+    left_stack = [left]
+    right_stack = [right]
+    while len(left_stack) > 0 and len(right_stack) > 0:
+        left = left_stack.pop()
+        right = right_stack.pop()
+        if left >= right:
+            continue
+        standard = un_sorted_arr[right]
+        i = left
+        j = right - 1
+        while True:
+            while un_sorted_arr[j] > standard:
+                j -= 1
+            while un_sorted_arr[i] < standard:
+                i += 1
+            if i < j:
+                un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[j]
+                un_sorted_arr[j] = un_sorted_arr[i] ^ un_sorted_arr[j]
+                un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[j]
+            else:
+                break
+
+        if i < right:
+            un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[right]
+            un_sorted_arr[right] = un_sorted_arr[i] ^ un_sorted_arr[right]
+            un_sorted_arr[i] = un_sorted_arr[i] ^ un_sorted_arr[right]
+
+        left_stack.append(left)
+        right_stack.append(i - 1)
+        left_stack.append(i + 1)
+        right_stack.append(right)
+
+
+# arr = [-100, 200, -5, 4000, 100, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10000, -1232131, 2343, 0, 1, 8]
+# arr = [-5,4,3,-2,2,1,0]
+arr = []
+for i in range(10):
+    arr.append(rd.randint(-100, 100))
+arr1 = arr.copy()
+print("原始数组 : ", arr)
+quick_sort_iteration(arr)
+print("循环排序结果 : ", arr)
+quick_sort(arr1, 0, len(arr1) - 1)
+print("递归排序结果 : ", arr1)
